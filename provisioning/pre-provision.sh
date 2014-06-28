c7d61fac870cd62217bc1993933c2b80b55fe1a3
Steve Oberther <stephenoberther@gmail.com>
2014-06-28 17:16:40 -0400

    Cleanup pre-provisioning to do the bare minimum before ansible playbook can run

--- provisioning/pre-provision.sh
+++ provisioning/pre-provision.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 ## Setup Root Keys
-[ ! -d /root/.ssh ] && mkdir /root/.ssh
-cp /vagrant/files/authorized_keys /root/.ssh/authorized_keys
-chown root: /root/.ssh/authorized_keys
-chmod 600 /root/.ssh/authorized_keys
+###[ ! -d /root/.ssh ] && mkdir /root/.ssh
+###cp /vagrant/files/authorized_keys /root/.ssh/authorized_keys
+###chown root: /root/.ssh/authorized_keys
+###chmod 600 /root/.ssh/authorized_keys
 
 ## Install some software
-apt-get update
-apt-get install -y ansible git tmux python-pycurl python-pip
-pip install docker-py
+echo "Updating apt"
+apt-get update 2>&1 > /dev/null
+apt-get install -y python-pycurl
+###apt-get install -y ansible git tmux python-pycurl python-pip
+#pip install docker-py
