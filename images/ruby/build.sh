3408fa6b5f153f7e137fb547cc733b4ffef6518c
Steve Oberther <stephenoberther@gmail.com>
2014-06-29 10:33:19 -0400

    Fix broken template and write generated dockerfile out to disk. Don't check Dockerfile into git and remove our .deb package we accidentally checked in

--- images/ruby/build.sh
+++ images/ruby/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ./make.rb $1
-exit
 docker build -t ruby .
 docker run -i -t -v `pwd`:/tmp ruby cp /ruby-1.9.3-p484.deb /tmp/
 ls -la ./ruby-1.9.3-p484.deb
--- images/ruby/make.rb
+++ images/ruby/make.rb
@@ -3,8 +3,12 @@ require 'erb'
 
 version = ARGV[0]
 version ||= '1.9.3-p484'
+
+ver_index = version.split('-')[0].index('.', 2)
+base_ver = version[0, ver_index]
+
 template = File.new("Dockerfile.erb", "r").read
 renderer = ERB.new(template)
 
-puts output = renderer.result()
+File.new("Dockerfile", "w+").write renderer.result
 
