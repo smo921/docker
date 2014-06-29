ffdb13572d1c71f85ffe537e9e89558af77f8794
Steve Oberther <stephenoberther@gmail.com>
2014-06-29 11:12:24 -0400

    Cleanup old commented stuff out of pre-provision script

--- provisioning/pre-provision.sh
+++ provisioning/pre-provision.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-## Setup Root Keys
-###[ ! -d /root/.ssh ] && mkdir /root/.ssh
-###cp /vagrant/files/authorized_keys /root/.ssh/authorized_keys
-###chown root: /root/.ssh/authorized_keys
-###chmod 600 /root/.ssh/authorized_keys
-
 ## Install some software
 echo "Updating apt"
 apt-get update 2>&1 > /dev/null
 apt-get install -y python-pycurl
-###apt-get install -y ansible git tmux python-pycurl python-pip
-#pip install docker-py
