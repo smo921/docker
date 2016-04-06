a02e70261a2f0d9d8e47a0bc13e798d584633c96
Stephen Oberther <stephenoberther@gmail.com>
2016-04-06 18:25:20 -0400

    Update to the latest version and move over to alpine linux

--- images/kafka/run.sh
+++ images/kafka/run.sh
@@ -10,7 +10,7 @@ if [ ! -z "$ZK_SERVERS" ]; then
   for i in $( eval echo {2..$ZK_SERVERS}); do\
     ZK_CONNECT="$ZK_CONNECT,zookeeper-$i:2181"
   done
-  echo $ZK_CONNECT >> $CONF
+  echo "${ZK_CONNECT}${KAFKA_PATH}" >> $CONF
 else
   echo "zookeeper.connect=zookeeper:2181" >> $CONF
 	echo "Starting up in standalone mode"
--- images/zk-cli/Dockerfile.java
+++ images/zk-cli/Dockerfile.java
@@ -1,14 +1,16 @@
-FROM java:openjdk-8u66-jre
+FROM java:8-jre-alpine
 
 MAINTAINER soberther@gmail.com
 
-ENV REFRESH_DATE 2015-09
-ENV ZOOKEEPER_VERSION 3.4.6
+ENV REFRESH_DATE 2016-04
+ENV ZOOKEEPER_VERSION 3.4.8
+
+RUN apk update && apk add bash
 
 RUN wget -q \
- http://mirror.metrocast.net/apache/zookeeper/stable/zookeeper-3.4.6.tar.gz
-RUN tar xzf zookeeper-3.4.6.tar.gz
+ http://mirror.metrocast.net/apache/zookeeper/stable/zookeeper-3.4.8.tar.gz
+RUN tar xzf zookeeper-3.4.8.tar.gz
 
-WORKDIR /zookeeper-3.4.6/bin
+WORKDIR /zookeeper-3.4.8/bin
 
 ENTRYPOINT ["./zkCli.sh", "-server"]
