56c29018852c1d4e9d0de7d050590ce7b9038fd0
Stephen Oberther <stephenoberther@gmail.com>
2016-04-17 22:57:35 -0400

    Set HA advertised address via env var

--- images/vault/run.sh
+++ images/vault/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ETH0_ADDR=$(ip -f inet -o addr| grep eth0 | awk '{print $4}' | awk -F'/' '{ print $1 }')
+export VAULT_ADVERTISE_ADDR="http://${ETH0_ADDR}:8200/"
 
 sed -i'' -e "s/CONSUL_SERVER/${CONSUL_SERVER}/" /etc/vault/vault.hcl
 
